9b697cb60b9795e431e62a5337905e8ac52c99c4
Christine Parisek <christineparisek@Christines-MacBook-Pro.local>
2019-03-11 01:06:32 -0700

    wk 9 sunday night ahh

---
+++ scripts/week_9_assignment_CAP.R
@@ -0,0 +1,24 @@
+#This is your last assignment for the R-DAVIS course! We hope you have gotten a good overview of all the amazing tools R can offer for your graduate work (and beyond!) during the past 10 weeks.
+
+#For your homework this week, we are going to be working with Rmarkdown and iteration, the two topics we covered last class.
+
+
+# Part 1: Rmarkdown -------------------------------------------------------
+
+
+
+Create a new Rmarkdown document and save it as week9_assignment_ABC.Rmd with your initals in place of ABC.
+
+Tell your coding partner about your research! Write a small paragraph about what you think (or know) your central research question might be for your PhD or Masters. Bold your main question, italisize the name of your study species, and be sure to include a picture of your study species/study site/or another picture.
+
+Use the storms data (included in RStudio) to create a plotly graph of the relationship between wind and pressure, where the status of the storm is indicated by a color.
+
+Create a table that identifies the mean wind, pressure, ts_diameter, hu_diameter of each status of storm (remember to remove NAs!). Use the package htmlTable. Round each mean to only two decimal places (Hint
+                                                                                                                                                                                                           look up the function round)
+CHALLENGE Find the duration, in number of days, of every hurricane from 2010 and later, and then use one of the map functions from purrr to write a sentence saying “Hurricane X lasted Y days” for each of these storms. You can look for some help with these functions here and here.
+
+Knit your Rmrkdown document. Hide the messages from appearing in the knited document. Save and push to Github.
+
+Pull down your partner’s Rmarkdown and see if you can knit the document on your computer.
+
+Answers:
